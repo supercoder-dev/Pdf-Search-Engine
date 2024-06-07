54a08db7b430abac77b9a521b03030719f4b7da8
supercoder-dev <mukunda@superagi.com>
2024-06-07 18:21:31 +0530

    Update pdfReader.py

--- pdfReader.py
+++ pdfReader.py
@@ -1,43 +1,47 @@
 import PyPDF2
 import json
- 
-#create file object variable
-#opening method will be rb
-pdffileobj=open('AU127-1.pdf','rb')
- 
-#create reader variable that will read the pdffileobj
-pdfreader=PyPDF2.PdfReader(pdffileobj)
- 
-#This will store the number of pages of this pdf file
-x=len(pdfreader.pages)
- 
-#create a variable that will select the selected number of pages
-# pageobj=pdfreader.pages[x-1]
- 
-#(x+1) because python indentation starts with 0.
-#create text variable which will store all text datafrom pdf file
-# text=pageobj.extract_text()
+
+pdffileobj = open('AU127-1.pdf', 'rb')
+pdfreader = PyPDF2.PdfReader(pdffileobj)
+x = len(pdfreader.pages)
 
 text = ""
-
-print(type(text))
-
 for i in range(x):
-    pageobj=pdfreader.pages[i-1]
+    pageobj = pdfreader.pages[i]
     pg = pageobj.extract_text()
-    text += "/n"
+    text += "\n"
     text += pg
 
- 
-#save the extracted data from pdf to a txt file
-#we will use file handling here
-#dont forget to put r before you put the file path
-#go to the file location copy the path by right clicking on the file
-#click properties and copy the location path and paste it here.
-#put "\\your_txtfilename"
+with open(r"AU127-1.txt", "w") as file1:
+    file1.writelines(text)
 
-file1=open(r"AU127-1.txt","a")
+metadata = pdfreader.metadata
+pdf_metadata = {
+    "Author": metadata.get('/Author', 'Unknown'),
+    "Title": metadata.get('/Title', 'Unknown'),
+    "CreationDate": metadata.get('/CreationDate', 'Unknown')
+}
+
+data = {
+    "Metadata": pdf_metadata,
+    "Content": text
+}
+
+with open("text.json", 'w') as f:
+    json.dump(data, f, indent=4)
+
+print("PDF Metadata:")
+print(json.dumps(pdf_metadata, indent=4))
+
+pdffileobj.close()
+
+file1 = open(r"AU127-1.txt", "a")
 file1.writelines(text)
+json_data = json.dumps(data)
+
+f = open("text.json", 'w')
+f.write(json_data)
+f.close()
 
 json = json.dump(file1)
 
