0e7ebd8a53358b7e1a12d00398670ae1a8f712d1
muskaancontlo <157454647+muskaancontlo@users.noreply.github.com>
2024-06-07 18:27:40 +0530

    Update pdfReader.py

--- pdfReader.py
+++ pdfReader.py
@@ -1,46 +1,36 @@
 import PyPDF2
 import json
- 
-#create file object variable
-#opening method will be rb
-pdffileobj=open('AU127-1.pdf','rb')
- 
-#create reader variable that will read the pdffileobj
-pdfreader=PyPDF2.PdfReader(pdffileobj)
- 
-#This will store the number of pages of this pdf file
-x=len(pdfreader.pages)
- 
-#create a variable that will select the selected number of pages
-# pageobj=pdfreader.pages[x-1]
- 
-#(x+1) because python indentation starts with 0.
-#create text variable which will store all text datafrom pdf file
-# text=pageobj.extract_text()
+
+pdffileobj = open('AU127-1.pdf', 'rb')
+pdfreader = PyPDF2.PdfFileReader(pdffileobj)
+x = pdfreader.numPages
 
 text = ""
-
-print(type(text))
-
 for i in range(x):
-    pageobj=pdfreader.pages[i-1]
+    pageobj = pdfreader.getPage(i)
     pg = pageobj.extract_text()
-    text += "/n"
+    text += "\n"
     text += pg
 
- 
-#save the extracted data from pdf to a txt file
-#we will use file handling here
-#dont forget to put r before you put the file path
-#go to the file location copy the path by right clicking on the file
-#click properties and copy the location path and paste it here.
-#put "\\your_txtfilename"
+with open("AU127-1.txt", "w") as file1:
+    file1.write(text)
 
-file1=open(r"AU127-1.txt","a")
-file1.writelines(text)
+metadata = pdfreader.getDocumentInfo()
+pdf_metadata = {
+    "Author": metadata.author if metadata.author else 'Unknown',
+    "Title": metadata.title if metadata.title else 'Unknown',
+    "CreationDate": metadata['/CreationDate'] if '/CreationDate' in metadata else 'Unknown'
+}
 
-json = json.dump(file1)
+data = {
+    "Metadata": pdf_metadata,
+    "Content": text
+}
 
-f = open("text.json", 'w')
-f.write(json)
-f.close()
+with open("text.json", 'w') as f:
+    json.dump(data, f, indent=4)
+
+print("PDF Metadata:")
+print(json.dumps(pdf_metadata, indent=4))
+
+pdffileobj.close()
